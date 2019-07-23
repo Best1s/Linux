0387241c473032cf62af63b497e084c91f6f6a4e
BaniZhang <987066696@qq.com>
2019-07-23 12:04:16 +0800

    add nginx

--- Nginx/install_nginx/nginx1.14_yum_install.sh
+++ Nginx/install_nginx/nginx1.14_yum_install.sh
@@ -2,7 +2,7 @@
 #dep pcre-devel  zlib-devel
 yum -y install yum-utils
 cp ./nginx.repo /etc/yum.repos.d/
-yum-config-manager --enable nginx-mainline
+yum -config-manager --enable nginx-mainline
 yum -y install nginx
 #http://nginx.org/en/linux_packages.html#RHEL-CentOS
 #http://nginx.org/en/linux_packages.html#RHEL-CentOS
